34265cb4ef5ae40523950d55ef20077823db042e
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-07-01 10:42:08 +0300

    NEW

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import About from "./pages/about";
+import About from "./pages/Aboutbout";
 import {
   createBrowserRouter,
   RouterProvider,
